f90185d3d4649991371d21dba1849536ff437ee8
Jose Ramirez <webmaster@reapbooster.com>
2026-06-27 01:57:48 -0700

    fix: Fix bug after VSCode v1.123

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,4 +1,31 @@
 const { fs } = require('memfs');
 
+// Patch memfs FsReadStream and FsWriteStream to respect autoClose: false in _destroy.
+// In memfs, _destroy unconditionally calls this.close() which closes the fd even if autoClose is false.
+if (fs.ReadStream && fs.ReadStream.prototype) {
+  fs.ReadStream.prototype._destroy = function (err, cb) {
+    if (this.autoClose) {
+      this.close(err2 => {
+        cb(err || err2);
+      });
+    } else {
+      cb(err);
+    }
+  };
+}
+
+if (fs.WriteStream && fs.WriteStream.prototype) {
+  fs.WriteStream.prototype._destroy = function (err, cb) {
+    if (this.autoClose) {
+      this.close(err2 => {
+        cb(err || err2);
+      });
+    } else {
+      cb(err);
+    }
+  };
+}
+
 fs.__mock__ = true;
 module.exports = fs;
+
--- src/commands/abstract/createCommand.ts
+++ src/commands/abstract/createCommand.ts
@@ -3,6 +3,7 @@ import logger from '../../logger';
 import { reportError } from '../../helper';
 import { handleCtxFromUri, allHandleCtxFromUri, FileHandlerContext } from '../../fileHandlers';
 import Command from './command';
+import { COMMAND_UPLOAD_FILE_TO_ALL_PROFILES, COMMAND_UPLOAD_FOLDER_TO_ALL_PROFILES } from '../../constants';
 
 interface BaseCommandOption {
   id: string;
